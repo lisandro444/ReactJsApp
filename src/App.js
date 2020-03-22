615f49423eef0fc3fd8d3a0dab3bfa8f09f9023f
Lisandro Rossi <lisandrorossi444@gmail.com>
2020-03-22 01:25:12 -0300

    using state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+// function Hello(props) {
+
+//   return <h2>{props.title}</h2>
+// };
+
+// arrow function
+const Hello = props => <h2>{props.title}</h2>
+
+function Text(props) {
+  const {
+    isActivated,
+    textbool,
+    ObjectWithInfo } = props
+
+  return <div>
+    <p>{ObjectWithInfo.key} </p>
+    <p>{textbool}</p>
+    <p>{isActivated}</p>
+  </div>
+}
+
+class Contador extends Component {
+  constructor() {
+    super();
+    this.state = { contador: 1 };
+  }
+  render() {
+    return <p>{this.state.contador}</p>
+  }
+}
+
+
 
 function App() {
   return (
@@ -8,16 +41,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Hello
+            title="Hello React World!!!"
+          />
+          <Text
+            ObjectWithInfo={{ key: "First Value", key2: "Value2" }}
+            isActivated={false}
+          />
+          <Contador  ></Contador>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
